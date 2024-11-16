61bd9346128ca208272b1e904c7680d8ba5d2e65
BikashPandeya <bikashpandeya5@gmail.com>
2024-11-16 17:52:06 +0000

    container config done

---
+++ basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountemail = "bikash@meta.com"
+var accountpass = "12345"
+accountcity = "Pokhara"
+
+
+// accountId = 2 
+/*cant change the value of const character
+  also never use var and never left variables open . Always use let*/
+console.log(accountId);
+
+accountemail = "harry@gmail.com"
+accountpass = "qwerty"
+accountcity = "Kathmandu"
+console.table([accountemail , accountpass , accountcity]);
+
