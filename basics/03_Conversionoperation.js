8e3a3c9c4b1ea83af3906f33213c1507b620e577
BikashPandeya <bikashpandeya5@gmail.com>
2024-11-17 15:25:05 +0000

    conversion operations js

---
+++ basics/03_Conversionoperation.js
@@ -0,0 +1,94 @@
+let name_ = "bikash"
+let no = 11
+let number_string = "11abc"
+let one = 1
+let zero = 0
+
+console.log(acc = typeof name_ );
+console.log(acc = typeof no );
+console.log(acc = typeof number_string );
+console.log(acc = typeof one  );
+console.log(acc = typeof zero , "\n\n");
+
+console.log(acc = Number( name_));
+console.log(typeof acc , "\n" );
+console.log(acc = Number( no));
+console.log(typeof acc , "\n" );
+console.log(acc = Number( number_string));
+console.log(typeof acc , "\n" );
+console.log(acc = Number( one));
+console.log(typeof acc , "\n" );
+console.log(acc = Number( zero) , );
+console.log(typeof acc , "\n\n\n" );
+
+
+console.log(acc = Boolean( name_));
+console.log(typeof acc , "\n" );
+console.log(acc = Boolean( no));
+console.log(typeof acc , "\n" );
+console.log(acc = Boolean( number_string));
+console.log(typeof acc , "\n" );
+console.log(acc = Boolean( one));
+console.log(typeof acc , "\n" );
+console.log(acc = Boolean( zero) , );
+console.log(typeof acc , "\n\n\n" );
+
+
+console.log(acc = String( name_));
+console.log(typeof acc , "\n" );
+console.log(acc = String( no));
+console.log(typeof acc , "\n" );
+console.log(acc = String( number_string));
+console.log(typeof acc , "\n" );
+console.log(acc = String( one));
+console.log(typeof acc , "\n" );
+console.log(acc = String( zero));
+console.log(typeof acc , "\n" );
+
+
+console.log(typeof undefined)
+console.log(typeof null , "\n")
+
+console.log( undefined)
+console.log( null)
+
+
+//**********************************************************Operations************************************************** */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
