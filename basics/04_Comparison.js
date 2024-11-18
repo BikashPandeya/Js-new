08edf0c6066489773c1b2ae5c5a3526749e37737
BikashPandeya <bikashpandeya5@gmail.com>
2024-11-18 13:40:25 +0000

    Comparison datatype

---
+++ basics/04_Comparison.js
@@ -0,0 +1,16 @@
+console.log("2" >1);
+console.log("02" >1 , "\n\n");
+
+console.log(null >0);
+console.log(null <0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null == 0 , "\n\n");
+
+console.log(undefined >0);
+console.log(undefined <0);
+console.log(undefined >= 0);
+console.log(undefined >= 0);
+console.log(undefined == 0 , "\n\n");
+
+console.log("2"===2) //=== means strict epual comparison which does not changes datatype of 2 into no. as == does .  
