332479e65b32652c7db28f8f03c1cce51ff3dd01
BikashPandeya <bikashpandeya5@gmail.com>
2024-11-18 14:06:11 +0000

    Datatypes summary

--- basics/05_datatypes_summary.js
+++ basics/05_datatypes_summary.js
@@ -42,7 +42,23 @@ console.log(obj1.name); // Output: Bob (affected by the change)
 let x = 10;
 let y = x; // Copy of the value
 y = 20;
-console.log(x); // Output: 10 (unchanged)
+console.log(x , "\n\n"); // Output: 10 (unchanged)
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// stack memory = (primitave datatype)    and   heap memory = (non-primative datatype)
+
+
+let objone = { name: "Alice" };
+let objtwo = objone; // Reference is copied
+objtwo.name = "Bob";
+console.log(objone.name); // Output: Bob (affected by the change)
+console.log(objtwo.name , "\n");
+
+
+let a = 10;
+let b = x; // Copy of the value
+b = 20;
+console.log(a);// Output: 10 (unchanged)
+console.log(b);
