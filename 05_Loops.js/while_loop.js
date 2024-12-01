0f6b42c4cee95aac0b39483b4c5d7a90a899a81c
BikashPandeya <bikashpandeya5@gmail.com>
2024-12-01 06:37:49 +0000

    While loop

---
+++ 05_Loops.js/while_loop.js
@@ -0,0 +1,22 @@
+
+let index = 0
+while (index<=10) {
+    console.log(`Value of index is : ${index}`);
+    index = index+2
+}
+
+let myarray = ['batman' , 'Superman' , 'Ironman' , 'Spiderman']
+a = 0
+while(a < myarray.length){
+    console.log(myarray[a])
+    a++
+}
+
+
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score <= 10);
